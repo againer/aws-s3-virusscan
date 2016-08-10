a4d6ccd7260ba589f4997232cb2809c7b468beb9
Al Pal <superrawr@gmail.com>
2016-08-09 18:10:44 -0700

    revert worker.rb and rubocop it

--- worker.rb
+++ worker.rb
@@ -1,63 +1,62 @@
 #!/usr/bin/env ruby
 
 require 'aws-sdk'
+require 'English'
 require 'json'
-require 'syslog/logger'
 require 'uri'
 require 'yaml'
+require 'syslog/logger'
 
-require './asset.rb'
-require './scanner.rb'
-
-TEMP_FILE = '/tmp/target'.freeze
-log = Syslog::Logger.new('s3-virusscan')
+log = Syslog::Logger.new 's3-virusscan'
 conf = YAML.load_file(__dir__ + '/s3-virusscan.conf')
 
-# Update the aws config with the s3-virusscan.conf.
 Aws.config.update(region: conf['region'])
-
+s3 = Aws::S3::Client.new
 sns = Aws::SNS::Client.new
 
 poller = Aws::SQS::QueuePoller.new(conf['queue'])
 
-log.info('s3-virusscan started')
+log.info 's3-virusscan started'
 
 poller.poll do |msg|
   body = JSON.parse(msg.body)
-  next unless body.key?('Records')
+  if body.key?('Records')
+    body['Records'].each do |record|
+      bucket = record['s3']['bucket']['name']
+      key = URI.decode(record['s3']['object']['key']).tr('+', ' ')
+      log.debug "scanning s3://#{bucket}/#{key}..."
+      begin
+        s3.get_object(response_target: '/tmp/target', bucket: bucket, key: key)
+      rescue Aws::S3::Errors::NoSuchKey
+        log.debug "s3://#{bucket}/#{key} does no longer exist"
+        next
+      end
+      if system('clamscan /tmp/target')
+        log.debug "s3://#{bucket}/#{key} was scanned without findings"
+      end
+      message = "s3://#{bucket}/#{key} is infected"
+      message = "s3://#{bucket}/#{key} is infected, deleting." if conf['delete']
+      log.error(message)
 
-  # Scan each record available.
-  body['Records'].each do |record|
-    bucket = record['s3']['bucket']['name']
-    key = URI.decode(record['s3']['object']['key']).tr('+', ' ')
-    asset = Asset.new(bucket, key, TEMP_FILE, log)
+      # Delete the asset.
+      if conf['delete']
+        s3.delete_object(bucket: bucket, key: key)
+        log.error("s3://#{bucket}/#{key} was deleted")
+      end
 
-    # Set bucket and key for getting a bucket item.
-    next unless asset.persist_local?
-
-    next if Scanner.new.virus?(bucket, key, TEMP_FILE, log)
-    # Scan the asset.
-    message = "s3://#{bucket}/#{key} is infected"
-    message = "s3://#{bucket}/#{key} is infected, deleting..." if conf['delete']
-    log.error(message)
-
-    # Delete the asset.
-    if conf['delete']
-      log.error("s3://#{bucket}/#{key} was deleted")
-      Asset.delete_remote
-    end
-
-    # Publish to the SNS topic.
-    sns.publish(
-      topic_arn: conf['topic'],
-      message: message,
-      subject: "s3-virusscan s3://#{bucket}",
-      message_attributes: {
-        'key' => {
-          data_type: 'String',
-          string_value: "s3://#{bucket}/#{key}"
+      # Publish to the SNS topic.
+      sns.publish(
+        topic_arn: conf['topic'],
+        message: message,
+        subject: "s3-virusscan s3://#{bucket}",
+        message_attributes: {
+          'key' => {
+            data_type: 'String',
+            string_value: "s3://#{bucket}/#{key}"
+          }
         }
-      }
-    )
+      )
+      system('rm /tmp/target')
+    end
   end
 end
